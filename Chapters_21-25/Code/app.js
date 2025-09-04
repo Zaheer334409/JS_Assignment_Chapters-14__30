6dbfa8dcda6e058348bde9b7509fa11cc6d187fc
Zaheer Ali <zaheerali334409@gmail.com>
2025-09-04 11:32:16 +0500

    One_File

---
+++ Chapters_21-25/Code/app.js
@@ -0,0 +1,191 @@
+// console.log("Hello World");
+
+//  01_Complated Question.
+// __________________________________
+
+// let firstname = prompt("Enter FirstName");
+// let lastname = prompt("Enter LastName");
+// let user = firstname + "  " + lastname;
+// alert(user + "WelCome! User");
+
+//  02_Complated Question. +index_file
+// __________________________________
+
+// let phone = document.getElementById("phone");
+// let gb = document.getElementById("gb");
+
+// let phonePara = document.getElementById("phonePara");
+// let phoneGb = document.getElementById("phoneGb");
+
+
+// function add() {
+//     if (phone.value == "" || gb.value == "") {
+//         phoneGb.innerHTML = "Empty Input";
+//         phoneGb.style.color = "red";
+//     }
+//     else {
+//         phonePara.innerHTML = phone.value;
+//         phonePara.style.color = "green";
+//         phoneGb.innerHTML = gb.value;
+//         phoneGb.style.color = "green";
+//     }
+//     phone.value = ""
+//     gb.value = ""
+// }
+
+// 02_Complated Question
+// __________________________________
+
+// let mobileModel = prompt("Enter your favorite mobile phone model:");
+// let length = mobileModel.length;
+
+// alert("Your favorite mobile phone is: " + mobileModel + "\nLength of input: " + length);
+
+
+// 03_Complated Question
+// __________________________________
+
+// let string = "Pakistan";
+// console.log(string.indexOf("n"));
+
+// 04_Complated Question
+
+// let variable = "Hello World";
+// console.log(variable.lastIndexOf("l"));
+
+// 05_Complated Question
+// __________________________________
+
+// let variable = "Pakistani";
+// let checkIndex = variable.indexOf("i");
+// let charAtvalue = variable.charAt("3");
+// console.log(checkIndex, charAtvalue);
+
+// 06_Complated Question
+// __________________________________
+
+// let variable = "Pakistani";
+
+// let checkIndex = variable.indexOf("i");
+// let charAtvalue = variable.concat("3");
+// console.log(charAtvalue);
+
+// 07_Complated Question +index__file
+
+// let changeText = document.getElementById("change");
+// changeText.innerHTML = "Islamabad"
+
+
+// 08_Complated Question  .... Samj nhi aya code AI kiya hai
+// __________________________________
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var newMessage = message.replace(/and/g, "&");
+// document.writeln(newMessage);
+
+// 09_Complated Question
+// __________________________________
+
+// let num_1 = 432423;
+// let changetype_1 = String(num_1);
+// console.log(typeof (changetype_1));
+
+// let num_2 = "432423";
+// let changetype_2 = Number(num_2);
+// console.log(typeof (changetype_2));
+
+
+//  10_Complated Question
+// __________________________________
+
+// let change  = "pakistan";
+// console.log(change.toUpperCase());  //console
+// document.writeln(change.toUpperCase());
+
+// 11_Complated Question
+// __________________________________
+
+// let userInput = "javascript";
+// let upper = userInput.charAt(0).toUpperCase() + userInput.slice(1)
+
+// console.log("Upper case: " + upper);
+// document.writeln("Upeer Case : " + upper);
+
+
+// 12_Complated Question
+// __________________________________
+
+// var num = 35.36;
+
+// var str = num.toString();
+
+// var result = str.split(".").join("");
+
+// document.write("Result: " + result);
+// console.log("Result:", result);
+
+// 13_Complated_Question
+// __________________________________
+
+// Samj nhi aya Sir ?
+
+
+// 14_Complated_Question
+// __________________________________
+
+// Array of items
+// let A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// let userInput = prompt("Welcome! What do you want to order?");
+// let itemFound = false;
+
+// for (let i = 0; i < A.length; i++) {
+//     if (A[i].toLowerCase() === userInput.toLowerCase()) {
+//         itemFound = true;
+//         break;
+//     }
+// }
+
+// if (itemFound) {
+//     alert(userInput + " is available at our bakery");
+// } else {
+//     alert("We are sorry, " + userInput + " is not available at our bakery");
+// }
+
+
+// 15_Question
+// __________________________________
+
+//nhi howa hai
+// 16_Question
+
+// var university = "U n i v e r s i t y o f K a r a c h i";
+
+// var arr = university.split(" ");
+
+// for (var i = 0; i < arr.length; i++) {
+//     document.write(arr[i] + "<br>");
+// }
+
+// 17_Question
+// __________________________________
+
+
+// let pak = "Pakistan";
+// let pakInput = pak[pak.length-1];
+// console.log(pakInput);
+
+// 18_Question
+// __________________________________
+
+
+// let str = "The quick brown fox jumps over the lazy dog";
+// str = str.toLowerCase();
+// let word = str.split(" ")
+// let count = 0;
+// for (let i = 0; i < word.length; i++) {
+//     if(word[i] === "the"){
+//         count++
+//     }
+// }
+// console.log(count);
+// Assignment Done 
